9b7f49e9d72f0aa72589a34d8be1aa7d6c41d02a
Regina Esparza <Regina.Esparza@volvo.com>
2026-06-13 19:19:38 -0300

    feat: register account service dependency

--- Program.cs
+++ Program.cs
@@ -1,14 +1,17 @@
+using Banking_API.Services;
+using Banking_API.Services.Interfaces;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 
+builder.Services.AddSingleton<IAccountService, AccountService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
